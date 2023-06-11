7b53d2deadd449342da86e4db31829cf7215ccf8
Yutaro Mikasa <48838160+yuutaro@users.noreply.github.com>
2023-06-11 08:37:39 +0000

    Finish ch09

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_01_002831) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_11_055931) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -32,9 +32,25 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not is_logged_in?
     assert_response :see_other
     assert_redirected_to root_url
+    # 2番目のウィンドウでログアウトをクリックするユーザーをシュミレートする
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: "1")
+    assert_not_empty cookies[:remember_token]
+  end
+
+  test "login without remembering" do
+    # クッキーを保存してログイン
+    log_in_as(@user, remember_me: "1")
+    delete logout_path
+    # クッキーが削除されていることをを確認してからログイン
+    log_in_as(@user, remember_me: "0")
+    assert_empty cookies[:remember_token]
   end
 end
