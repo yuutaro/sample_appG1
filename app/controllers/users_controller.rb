fb5aa56201933b5dd416693122a765cf4a07bc52
Yutaro Mikasa <48838160+yuutaro@users.noreply.github.com>
2023-06-11 05:03:58 +0000

    Finish ch08

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find_by(params[:id])
+    @user = User.find(params[:id])
   end
 
   def new
@@ -10,8 +10,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @user
+      reset_session
+      log_in @user
       flash[:success] = "Welcome to the Sample app!"
+      redirect_to @user
     else
       render 'new', status: :unprocessable_entity
     end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,11 +3,13 @@ require_relative "../config/environment"
 require "rails/test_help"
 
 class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
+  # 指定のワーカー数でテストを並列実行する
   parallelize(workers: :number_of_processors)
-
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  # test/fixtures/*.ymlのfixtureをすべてセットアップする
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  # テストユーザーがログイン中の場合にtrueを返す
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
