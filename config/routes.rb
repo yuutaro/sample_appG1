9528170bbcd6b4de28c4df7e405217722f58f393
Yutaro Mikasa <48838160+yuutaro@users.noreply.github.com>
2023-06-16 08:12:44 +0000

    Finish ch12

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
   get "/login",       to: "sessions#new"
   post "/login",      to: "sessions#create"
