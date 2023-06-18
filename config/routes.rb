ad2d58fdd876d0c7befe3414e86eaf5bb0cdd769
Yutaro Mikasa <48838160+yuutaro@users.noreply.github.com>
2023-06-18 14:51:04 +0000

    Finish ch14

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,17 @@ Rails.application.routes.draw do
   get "/contact",     to: "static_pages#contact",  as: "contact"
   get "/signup",      to: "users#new",             as: "signup"           
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+      # GET /users/1/following
+      # GET /users/1/followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 
   get "/login",       to: "sessions#new"
   post "/login",      to: "sessions#create"
