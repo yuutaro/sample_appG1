4e95adee6d1d97529066561f525def580a070036
Yutaro Mikasa <48838160+yuutaro@users.noreply.github.com>
2023-06-18 15:39:01 +0000

    email-verification-omit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,10 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    @user.activate
     if @user.save
-      @user.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
+      # @user.send_activation_email
+      # flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
     else
       render 'new', status: :unprocessable_entity
